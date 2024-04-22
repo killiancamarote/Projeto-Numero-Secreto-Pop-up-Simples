954e12131380116e3304243fb43383a65179d643
killiancamarote <kilobomi@gmail.com>
2024-04-22 11:03:34 -0300

    meu primeiro commit

---
+++ app.js
@@ -0,0 +1,28 @@
+alert("Boas vindas ao jogo do número secreto!");
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// esquanto chute não for igual ao numeroSecreto
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert (`Dica: o número secreto é menor que ${chute}`);
+        } else {
+            alert (`Dica: o número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`PARABÉNS!! Você acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+
